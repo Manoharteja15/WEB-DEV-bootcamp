9715a916bb56c54c47fa0efdaf2837e63a503a98
Boyina Manohar Teja <manohartejaboyina@gmail.com>
2026-08-13 15:25:34 +0530

    test javascript update

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express=require('express');
 const app = express();
+app.use(express.static(path.join(__dirname, 'Frontend')));
 const port = process.env.PORT || 8080;
 app.use(express.static("frontend"));
 app.listen(port, function() {
