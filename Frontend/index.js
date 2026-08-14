4a40b6449d572c5bfb9996f1eb4e5b5138dbc88d
Boyina Manohar Teja <manohartejaboyina@gmail.com>
2026-08-14 10:46:09 +0530

    new

--- Frontend/index.js
+++ Frontend/index.js
@@ -24,3 +24,29 @@ function ToggleUser() {
     userGender.innerHTML = users[currId].gender;
     userImage.src = users[currId].img;
 }
+function randomUser() {
+  fetch("https://randomuser.me/api")
+    .then(function (res) {
+      return res.json();
+    })
+    .then(function (data) {
+      const user = data.results[0];
+
+      const userName = document.getElementById("user-name");
+      const userGender = document.getElementById("user-gender");
+      const userImage = document.getElementById("user-image");
+
+      const newUserName =
+        user.name.first + " " + user.name.last;
+
+      const newUserGender = user.gender;
+      const newUserImage = user.picture.large;
+
+      userName.innerHTML = newUserName;
+      userGender.innerHTML = newUserGender;
+      userImage.src = newUserImage;
+    })
+    .catch(function (err) {
+      console.log("Error occurred:", err);
+    });
+}
